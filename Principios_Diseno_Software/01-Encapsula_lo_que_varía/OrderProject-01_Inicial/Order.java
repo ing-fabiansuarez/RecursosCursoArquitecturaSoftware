204ec9a22dbdf8f4b6549fdce53fffbde7a75652
Fabian Suarez <fsuarez120@unab.edu.co>
2026-02-16 15:30:18 -0500

    Encapsulamiento buen nivel

--- Principios_Diseno_Software/01-Encapsula_lo_que_varía/OrderProject-01_Inicial/Order.java
+++ Principios_Diseno_Software/01-Encapsula_lo_que_varía/OrderProject-01_Inicial/Order.java
@@ -3,6 +3,10 @@ import java.util.List;
 public class Order {
     private List<LineItem> lineItems;
     private String country;
+    private String state;
+    private String city;
+    
+    private TaxCalculator taxCalculator;
     
     public Order(List<LineItem> lineItems, String country) {
         this.lineItems = lineItems;
@@ -10,25 +14,25 @@ public class Order {
     }
    
     public double getOrderTotal() {
-        double total = 0.0;
-        for (LineItem item : this.getLineItems()) {
-            total += item.getPrice() * item.getQuantity();
-        }
-         // Se obtiene la tasa desde un método separado
-        total += total * getTaxRate(this.getCountry());
-        return total;
-    }
-    
-     // Método separado para obtener la tasa de impuesto
-    public double getTaxRate(String country) {
 
-        if ("US".equals(country)) {
-            return 0.07; // Impuesto sobre la venta de EUA
-        } else if ("EU".equals(country)) {
-            return 0.20; // IVA europeo
-        } else {
-            return 0.0;
+        double total = 0.0;
+
+        for (LineItem item : lineItems) {
+
+            double subtotal = item.getPrice() * item.getQuantity();
+
+            double taxRate = taxCalculator.getTaxRate(
+                    country,
+                    state,
+                    item.getProduct()
+            );
+
+            double tax = subtotal * taxRate;
+
+            total += subtotal + tax;
         }
+
+        return total;
     }
     
     public List<LineItem> getLineItems() {

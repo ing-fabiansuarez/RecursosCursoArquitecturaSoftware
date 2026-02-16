1b2b072079abf21325e9910f62a84d38daffa91c
Fabian Suarez <fsuarez120@unab.edu.co>
2026-02-16 15:21:06 -0500

    EEncapsulación a nivel de la clase

--- Principios_Diseno_Software/01-Encapsula_lo_que_varía/OrderProject-01_Inicial/Main.java
+++ Principios_Diseno_Software/01-Encapsula_lo_que_varía/OrderProject-01_Inicial/Main.java
@@ -11,29 +11,7 @@ public class Main {
         items.add(item2);
         Order order = new Order(items, "US");
         // Llamar al método dentro de la misma clase
-        double total = getOrderTotal(order);
+        double total = order.getOrderTotal();
         System.out.println("Total de la orden: $" + total);
-    }
-    // Método de lógica de negocio dentro del Main
-    public static double getOrderTotal(Order order) {
-        double total = 0.0;
-        for (LineItem item : order.getLineItems()) {
-            total += item.getPrice() * item.getQuantity();
-        }
-         // Se obtiene la tasa desde un método separado
-        total += total * getTaxRate(order.getCountry());
-        return total;
-    }
-    
-     // Método separado para obtener la tasa de impuesto
-    public static double getTaxRate(String country) {
-
-        if ("US".equals(country)) {
-            return 0.07; // Impuesto sobre la venta de EUA
-        } else if ("EU".equals(country)) {
-            return 0.20; // IVA europeo
-        } else {
-            return 0.0;
-        }
     }
 }
--- Principios_Diseno_Software/01-Encapsula_lo_que_varía/OrderProject-01_Inicial/Order.java
+++ Principios_Diseno_Software/01-Encapsula_lo_que_varía/OrderProject-01_Inicial/Order.java
@@ -3,12 +3,34 @@ import java.util.List;
 public class Order {
     private List<LineItem> lineItems;
     private String country;
-
+    
     public Order(List<LineItem> lineItems, String country) {
         this.lineItems = lineItems;
         this.country = country;
     }
+   
+    public double getOrderTotal() {
+        double total = 0.0;
+        for (LineItem item : this.getLineItems()) {
+            total += item.getPrice() * item.getQuantity();
+        }
+         // Se obtiene la tasa desde un método separado
+        total += total * getTaxRate(this.getCountry());
+        return total;
+    }
+    
+     // Método separado para obtener la tasa de impuesto
+    public double getTaxRate(String country) {
 
+        if ("US".equals(country)) {
+            return 0.07; // Impuesto sobre la venta de EUA
+        } else if ("EU".equals(country)) {
+            return 0.20; // IVA europeo
+        } else {
+            return 0.0;
+        }
+    }
+    
     public List<LineItem> getLineItems() {
         return lineItems;
     }
@@ -16,4 +38,5 @@ public class Order {
     public String getCountry() {
         return country;
     }
+    
 }
